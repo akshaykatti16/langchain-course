25e40dd36cab56a8dac60b0e90ff27456ce8002f
Akshay Katti <akshay.katti16@gmail.com>
2025-12-04 17:12:15 +0530

    vector ingestion and vector retrieval chain

--- ingestion.py
+++ ingestion.py
@@ -10,17 +10,29 @@ load_dotenv()
 
 if __name__ == "__main__":
     print("Ingesting...")
-    loader = TextLoader("/Users/edenmarco/Desktop/langchain-course/mediumblog1.txt")
+    loader = TextLoader("C:\\Users\\akshaykatti\\langchain-course\\mediumblog1.txt",
+                        encoding="utf-8",
+                        autodetect_encoding=True)
     document = loader.load()
 
     print("splitting...")
+    #each document has chunk of 1000 words
+    #for 1k chunk size, 20 sub documents were created
+    #for 1.5k chunk size, 13 sub documents were created
+    #for 2.5k chunk size, 8 sub documents were created
+    # higher the no of chunk size -> lower the no of sub documents
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    #list of documents
     texts = text_splitter.split_documents(document)
+
     print(f"created {len(texts)} chunks")
+    print(f"chunking & splitting completed")
+    print("***")
 
-    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    #embedding model
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small",openai_api_key=os.environ.get("OPENAI_API_KEY"))
 
-    print("ingesting...")
+    print("ingesting to vector database...")
     PineconeVectorStore.from_documents(
         texts, embeddings, index_name=os.environ["INDEX_NAME"]
     )
--- main_with_LCEL.py
+++ main_with_LCEL.py
@@ -2,6 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
@@ -12,6 +13,9 @@ from langchain_classic.chains.retrieval import create_retrieval_chain
 
 load_dotenv()
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
 
 if __name__ == "__main__":
     print(" Retrieving...")
@@ -21,25 +25,37 @@ if __name__ == "__main__":
 
     query = "what is Pinecone in machine learning?"
     chain = PromptTemplate.from_template(template=query) | llm
-    #llm generic answer without reading context
-    #result = chain.invoke(input={})
-    #print(result.content)
-
+    
     #initialize pinecone
     vectorstore = PineconeVectorStore(
         index_name=os.environ["INDEX_NAME"], embedding=embeddings
     )
 
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-    #https://smith.langchain.com/hub/langchain-ai/retrieval-qa-chat?organizationId=0e235dde-e29a-4c07-8feb-207a158b1f02
+    #using LCEL
+    
+    template="""Use the following pieces of context to answer the questions.
+    If you dont know the answer, just say you dont know.
+    use 3-4 sentences maximum and keep the answer concise.
+    {context}
+    
+    Question : {question}
+    
+    Helpful Answer : 
+    
+    """
 
-    #combine all docs
-    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
-    #create chain from vector db
-    retrival_chain = create_retrieval_chain(
-        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+
+    rag_chain = (
+        {
+            "context" : vectorstore.as_retriever() | format_docs,
+            "question" : RunnablePassthrough()
+        }
+        | custom_rag_prompt
+        | llm
+
     )
 
-    result = retrival_chain.invoke(input={"input": query})
+    res = rag_chain.invoke(query)
 
-    print(result)
+    print(res)
